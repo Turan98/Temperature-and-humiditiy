9c221a07333ab2292c70f263a456815d9241f0d5
Turan98 <48137040+Turan98@users.noreply.github.com>
2022-10-19 19:58:23 +0400

    test7

---
+++ TemperatureAndHumidity/Program.cs
@@ -0,0 +1,22 @@
+﻿class program {
+    public static bool temperature()
+    {
+      return true;
+    }
+
+     public static bool humidity()
+    {
+      return true;
+    }
+
+    static void Main(string[] args){
+        if(temperature() || humidity())
+        {
+            Console.WriteLine("Torn on conditioner");
+        }
+        else
+        {
+            Console.WriteLine("not need");
+        }
+    }
+}
